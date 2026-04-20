68566464adf6ced9fe8e867a46149ea178e36bb0
NaomiBolak <naomi47216@gmail.com>
2026-04-20 23:16:58 +0300

    change database

--- Server/CommunityCenter.API/CommunityCenter.Domain/Entities/RegistrationEvent.cs
+++ Server/CommunityCenter.API/CommunityCenter.Domain/Entities/RegistrationEvent.cs
@@ -5,10 +5,15 @@ namespace CommunityCenter.Domain.Entities
     public class RegistrationEvent
     {
         public int Id { get; set; }
+
         public int EventId { get; set; }
         public int SubscriberId { get; set; }
-        public int PlacesCount { get; set; }
+
+        // ✔ כמה כרטיסים
+        public int PlacesCount { get; set; } = 1;
+
         public DateTime RegistrationDate { get; set; } = DateTime.Now;
+
         public bool IsPaid { get; set; }
 
         public virtual Event Event { get; set; }
