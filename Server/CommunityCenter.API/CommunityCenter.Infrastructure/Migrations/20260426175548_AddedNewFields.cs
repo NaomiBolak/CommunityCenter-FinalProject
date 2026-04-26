0702a8785f6dd2574d9663240ae8d09eca7f0e85
NaomiBolak <naomi47216@gmail.com>
2026-04-26 20:56:40 +0300

    updates

---
+++ Server/CommunityCenter.API/CommunityCenter.Infrastructure/Migrations/20260426175548_AddedNewFields.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommunityCenter.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Locations_LocationId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_TargetAudiences_TargetAudienceId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RegistrationEvents_Events_EventId",
+                table: "RegistrationEvents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RegistrationEvents_Subscribers_SubscriberId",
+                table: "RegistrationEvents");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RegistrationEvents",
+                table: "RegistrationEvents");
+
+            migrationBuilder.RenameTable(
+                name: "RegistrationEvents",
+                newName: "EventRegistrations");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RegistrationEvents_SubscriberId",
+                table: "EventRegistrations",
+                newName: "IX_EventRegistrations_SubscriberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RegistrationEvents_EventId_SubscriberId",
+                table: "EventRegistrations",
+                newName: "IX_EventRegistrations_EventId_SubscriberId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TargetAudienceId",
+                table: "Events",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Events",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "Events",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Events",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Events",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EventRegistrations",
+                table: "EventRegistrations",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventRegistrations_Events_EventId",
+                table: "EventRegistrations",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventRegistrations_Subscribers_SubscriberId",
+                table: "EventRegistrations",
+                column: "SubscriberId",
+                principalTable: "Subscribers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Locations_LocationId",
+                table: "Events",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_TargetAudiences_TargetAudienceId",
+                table: "Events",
+                column: "TargetAudienceId",
+                principalTable: "TargetAudiences",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventRegistrations_Events_EventId",
+                table: "EventRegistrations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventRegistrations_Subscribers_SubscriberId",
+                table: "EventRegistrations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Locations_LocationId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_TargetAudiences_TargetAudienceId",
+                table: "Events");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EventRegistrations",
+                table: "EventRegistrations");
+
+            migrationBuilder.RenameTable(
+                name: "EventRegistrations",
+                newName: "RegistrationEvents");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EventRegistrations_SubscriberId",
+                table: "RegistrationEvents",
+                newName: "IX_RegistrationEvents_SubscriberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EventRegistrations_EventId_SubscriberId",
+                table: "RegistrationEvents",
+                newName: "IX_RegistrationEvents_EventId_SubscriberId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TargetAudienceId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagePath",
+                table: "Events",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RegistrationEvents",
+                table: "RegistrationEvents",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Locations_LocationId",
+                table: "Events",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_TargetAudiences_TargetAudienceId",
+                table: "Events",
+                column: "TargetAudienceId",
+                principalTable: "TargetAudiences",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RegistrationEvents_Events_EventId",
+                table: "RegistrationEvents",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RegistrationEvents_Subscribers_SubscriberId",
+                table: "RegistrationEvents",
+                column: "SubscriberId",
+                principalTable: "Subscribers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
