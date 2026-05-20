ef43493b829d4b51e9484256b815d90f347cdeaf
efrat brown <efrat3440@gmail.com>
2026-05-20 13:33:03 +0300

    feat: completed news and contact features for backend and frontend

--- Client/my-app/src/store/index.ts
+++ Client/my-app/src/store/index.ts
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import classesReducer from './slices/classesSlice';
+import contactReducer from './slices/contactSlice';
+import eventsReducer from './slices/eventsSlice';
+import newsReducer from './slices/newsSlice';
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+    reducer: {
+        auth: authReducer,
+        classes: classesReducer,
+        contact: contactReducer,
+        events: eventsReducer,
+        news: newsReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
